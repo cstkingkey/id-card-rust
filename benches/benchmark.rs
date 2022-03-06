c9047f7445aca699cac1dff6be6c2b83f291232b
John <john@JohndeAir.lan>
2022-03-06 12:48:11 +0800

    initialize hashmap from array

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -6,5 +6,5 @@ extern crate id_card as id_card_number;
 
 #[bench]
 fn bench_validate(b: &mut Bencher) {
-    b.iter(|| id_card_number::validate("440524188001010014"));
+    b.iter(|| id_card_number::validate("440524188001010014", true));
 }
