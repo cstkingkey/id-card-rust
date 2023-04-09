27c134b2c7bd3966be8087ae7ca11f92d5c0ff24
John <m@DESKTOP-7TRGB04.localdomain>
2023-04-09 09:14:01 +0800

    0.1.6

--- src/lib.rs
+++ src/lib.rs
@@ -35,13 +35,10 @@ pub fn validate(id_number: &str, validate_region: bool) -> bool {
     let mut id_number_string = id_number.to_string();
     let check_code = id_number_string.pop().unwrap();
     //transform the chars except the last to u32
-    let items: Vec<u32> = id_number_string
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect();
+    let items = id_number_string.chars().map(|c| c.to_digit(10).unwrap());
     let factors: [u32; 17] = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
     let mut sum = 0u32;
-    for (&x, &y) in items.iter().zip(&factors) {
+    for (x, &y) in items.zip(&factors) {
         sum += x * y;
     }
     let verify_code_expected = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
